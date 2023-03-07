085f3b277b99fcbe8f0e0b21be7368bac77ab95d
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-03-07 23:14:08 +0100

    Add content to top list page

--- src/pages/lists/TopList.vue
+++ src/pages/lists/TopList.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
+  <div class="max-width">
     <h1>Top Rated List</h1>
+      <div class="results">
+        <base-result v-for="result in store.state.topList.results" :key="result.id" :data="result"/>
+      </div>
   </div>
 </template>
 
-<script>
+<script setup>
+import { useStore } from 'vuex';
+import { onBeforeMount } from 'vue';
+import { ref } from 'vue';
+
+const page = ref(1)
+
+const store = useStore();
+
+onBeforeMount(() => {
+  store.dispatch('getTrendingMoviesList', { part1:'https://api.themoviedb.org/3/movie/top_rated?api_key=', part2: '&language=en-US&page=', page: page.value, savePath: 'topList'})
+});
 
 </script>
 
 <style lang="scss" scoped>
-
 </style>
