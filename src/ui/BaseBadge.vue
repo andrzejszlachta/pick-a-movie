f0e70cba66a4a13c3b76afb67287208b9cd8f989
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-03-08 22:42:22 +0100

    Create Base Badge component

---
+++ src/ui/BaseBadge.vue
@@ -0,0 +1,31 @@
+<template>
+  <span class="badge">
+    {{ badgeName }}
+  </span>
+</template>
+
+<script setup>
+import { useStore } from 'vuex';
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  badge: {
+    type: Number,
+    required: true,
+  },
+})
+
+const store = useStore()
+const badgeName = store.state.genres.find(obj => obj.id === props.badge).name
+</script>
+
+<style lang="scss" scoped>
+  .badge {
+    padding: 5px 7px;
+    border-radius: 7px;
+    background-color: #EB455F;
+    color: #fff;
+    margin-right: 5px;
+    font-weight: bold;
+  }
+</style>
