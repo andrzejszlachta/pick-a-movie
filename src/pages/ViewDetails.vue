6364473c5cb28e4e56f063448c205265732c1b9c
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-03-09 16:57:35 +0100

    Add API request for movie details for ViewDetails page

--- src/pages/ViewDetails.vue
+++ src/pages/ViewDetails.vue
@@ -1,4 +1,43 @@
 <template>
+  <div v-if="data.movie">
+    <p>Recipe details id: {{ props.id }}</p>
+    <h2>{{ data.movie.title }}</h2>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore()
+const props = defineProps({id: String})
+
+const data = reactive({})
+
+async function getDetails() {
+  await store.dispatch('getDetails', props.id)
+  data.movie = reactive(store.state.recipesDetails.find(obj => obj.id === +props.id))
+  console.log(data)
+  }
+ getDetails()
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+<!-- <template>
   <div>
     Recipe details id: {{ props.id }}
   </div>
@@ -6,12 +45,40 @@
 
 <script setup>
 import { defineProps } from 'vue';
+import { useStore } from 'vuex';
 
+const store = useStore()
 const props = defineProps({id: String})
 
-console.log(props.id)
+let data
+
+async function getDetails() {
+      const API = 'dcafa276c4fbb7347b91d1e1c1c50ae3'
+      const fullURL = `https://api.themoviedb.org/3/movie/${props.id}?api_key=${API}&language=en-US`
+
+      //dont call api request if already data stored locally
+      if (store.state.recipesDetails.find(obj => obj.id === props.id) !== undefined) {
+        // do nothing
+      } else {
+
+        let response = await fetch(fullURL);
+        const responseData = await response.json();
+        if (!response.ok) {
+          const error = new Error(responseData.message || 'Failed to fetch data!');
+          throw error
+        }
+        store.state.recipesDetails.push(responseData)
+      }
+      data = store.state.recipesDetails.find(obj => obj.id === +props.id)
+      console.log(data)
+    }
+  getDetails()
+
 </script>
 
 <style lang="scss" scoped>
 
-</style>
+</style> -->
+
+
+
--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,34 @@ export default createStore({
     popularList: {},
     topList: {},
     genres: {},
+    recipesDetails: [],
   },
   getters: {
   },
   mutations: {
   },
   actions: {
+    async getDetails(context, payload) {
+      const API = 'dcafa276c4fbb7347b91d1e1c1c50ae3'
+      const fullURL = `https://api.themoviedb.org/3/movie/${payload}?api_key=${API}&language=en-US`
+
+      //dont call api request if already data stored locally
+      if (context.state.recipesDetails.find(obj => obj.id === payload) !== undefined) {
+        // do nothing
+      } else {
+
+        let response = await fetch(fullURL);
+        const responseData = await response.json();
+        if (!response.ok) {
+          const error = new Error(responseData.message || 'Failed to fetch data!');
+          throw error
+        }
+        context.state.recipesDetails.push(responseData)
+      }
+    },
     async getGenresList(context) {
-      const fullURL = 'https://api.themoviedb.org/3/genre/movie/list?api_key=dcafa276c4fbb7347b91d1e1c1c50ae3&language=en-US'
+      const API = 'dcafa276c4fbb7347b91d1e1c1c50ae3'
+      const fullURL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API}&language=en-US`
 
       //dont call api request if already data stored locally
       if (context.state.genres.length !== undefined) return
