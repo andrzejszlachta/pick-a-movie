a25cce63ecffb0cc0318e558baabfe62195f0aed
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-03-12 02:01:25 +0100

    Add styling to details page

--- src/pages/ViewDetails.vue
+++ src/pages/ViewDetails.vue
@@ -1,50 +1,58 @@
 <template>
   <div class="result max-width" v-if="data.movie">
     <div class="result-container">
-      <img :src="'https://image.tmdb.org/t/p/w780' + data.movie.poster_path" alt="poster">
-      <div class="data">
-        <h2 class="title">{{ data.movie.title }}<span class="year"> ({{ data.movie.release_date.slice(0, 4) }})</span><span v-if="data.movie.adult"> 🔞</span></h2>
-        <p class="original_title" v-if="data.movie.original_language !== 'en'"><span class="lang">{{ data.movie.original_language }} </span>{{ data.movie.original_title }}</p>
-        <div class="badges" v-if="data.movie.genres && store.state.genres.length">
-          <base-badge v-for="badge in data.movie.genres" :key="badge.id" :badge="badge.id"></base-badge>
+      <h2 class="title">{{ data.movie.title }}<span class="year"> ({{ data.movie.release_date.slice(0, 4) }})</span><span v-if="data.movie.adult"> 🔞</span></h2>
+      <p class="original_title" v-if="data.movie.original_language !== 'en'"><span class="lang">{{ data.movie.original_language }} </span>{{ data.movie.original_title }}</p>
+      <img v-if="data.movie.poster_path" :src="'https://image.tmdb.org/t/p/w780' + data.movie.poster_path" alt="poster">
+      <div class="badges" v-if="data.movie.genres && store.state.genres.length">
+        <base-badge v-for="badge in data.movie.genres" :key="badge.id" :badge="badge.id"></base-badge>
+      </div>
+      <table>
+        <tr v-if="data.movie.status"><th>{{ data.movie.status }}: </th><td>{{ data.movie.release_date }}</td></tr>        
+        <tr v-if="data.movie.budget"><th>Budget: </th><td>{{ moneyToString(data.movie.budget) }} USD</td></tr>
+        <tr v-if="data.movie.revenue"><th>Revenue: </th><td>{{ moneyToString(data.movie.revenue) }} USD</td></tr>
+        <tr v-if="data.movie.revenue && data.movie.budget"><th>Balance: </th><td>{{ moneyToString(balance) }} USD</td></tr>
+        <tr v-if="data.movie.vote_average"><th>Average rating: </th><td><span class="rating" :class="rating">{{ data.movie.vote_average.toFixed(2) }}</span></td></tr>
+        <tr v-if="data.movie.vote_count"><th>Vote count: </th><td>{{ data.movie.vote_count }}</td></tr>
+        <tr v-if="data.movie.runtime"><th>Length: </th><td>{{ data.movie.runtime }} minutes</td></tr>
+      </table>
+      <div class="overview box" v-if="data.movie.overview">
+        <span>Overview:</span>
+        <p class="description">{{ data.movie.overview }}</p>
+      </div>
+      <div class="companies box" v-if="data.movie.production_companies">
+        <span class="box_header">Production companies:</span>
+        <div class="company" v-for="company in data.movie.production_companies" :key="`prod-${company.id}`">
+          <p><span class="lang">{{ company.origin_country }}</span>{{ company.name }}</p>
+          <img v-if="company.logo_path !== null" :src="`https://image.tmdb.org/t/p/w185${company.logo_path}`" alt="company logo">
         </div>
-        <div class="overview">
-          <span>Overview:</span>
-          <p class="description">{{ data.movie.overview }}</p>
-        </div>
-        <p class="release"><span>{{ data.movie.status }}: </span>{{ data.movie.release_date }}</p>
-        <div class="money">
-          <p class="budget" v-if="data.movie.budget">Budget: {{ moneyToString(data.movie.budget) }} USD</p>
-          <p class="budget" v-if="data.movie.revenue">Revenue: {{ moneyToString(data.movie.revenue) }} USD</p>
-          <p class="budget" v-if="data.movie.revenue && data.movie.budget">Balance: {{ moneyToString(balance) }} USD</p>
-        </div>
-        <div class="votes">
-          <p>Average rating: <span class="rating" :class="rating">{{ data.movie.vote_average }}</span></p>
-          <p>Vote count: {{ data.movie.vote_count }}</p>
-        </div>
-        <div class="length">Length: <span>{{ data.movie.runtime }} minutes</span></div>
-        <div class="companies" v-if="data.movie.production_companies">
-          <span>Production companies:</span>
-          <div class="company" v-for="company in data.movie.production_companies" :key="`prod-${company.id}`">
-            <img v-if="company.logo_path !== null" :src="`https://image.tmdb.org/t/p/w185${company.logo_path}`" alt="company logo">
-            <p><span>{{ company.origin_country }}</span>{{ company.name }}</p>
-          </div>
-          <div class="countries" v-if="data.movie.production_countries">
-            <p>Filming locations: </p>
-            <div class="country" v-for="country in data.movie.production_countries" :key="`country-${data.movie.id}-${country.iso_3166_1}`">
-            <span>{{ country.iso_3166_1 }} </span><span>{{ country.name }}</span>
-            </div>
-          </div>
-          <div class="languages" v-if="data.movie.spoken_languages">
-            <p>Spoken languages: </p>
-            <span class="language" v-for="language in data.movie.spoken_languages" :key="`country-${data.movie.id}-${language.iso_639_1}`">{{ language.english_name }}</span>
-          </div>
-        </div>
-
-        <div class="links">
-          <a v-if="data.movie.homepage" :href="data.movie.homepage">Movies Homepage</a>
-          <a v-if="data.movie.homepage" :href="`https://www.imdb.com/title/${data.movie.imdb_id}`">IMDb</a>
-        </div>
+      </div>
+      <div class="countries box" v-if="data.movie.production_countries">
+        <p>Filming locations: </p>
+        <table>
+          <tr v-for="country in data.movie.production_countries" :key="`country-${data.movie.id}-${country.iso_3166_1}`">
+            <th><span class="lang">{{ country.iso_3166_1 }} </span></th><td>{{ country.name }}</td>
+          </tr>
+        </table>
+      </div>
+      <div class="languages box" v-if="data.movie.spoken_languages">
+        <p>Spoken languages: </p>
+        <ul>
+          <li class="language" v-for="language in data.movie.spoken_languages" :key="`country-${data.movie.id}-${language.iso_639_1}`">{{ language.english_name }}</li>
+        </ul>
+      </div>
+      <div class="links box" v-if="data.movie.homepage || data.movie.imdb_id">
+        <p>Useful links:</p>
+        <ul>
+          <li><a v-if="data.movie.homepage" :href="data.movie.homepage">Movie's Homepage</a></li>
+          <li><a v-if="data.movie.imdb_id" :href="`https://www.imdb.com/title/${data.movie.imdb_id}`"> 
+            <svg id="home_img" class="ipc-logo" xmlns="http://www.w3.org/2000/svg" width="64" height="32" viewBox="0 0 64 32" version="1.1"><g fill="#F5C518"><rect x="0" y="0" width="100%" height="100%" rx="4"></rect></g><g transform="translate(8.000000, 7.000000)" fill="#000000" fill-rule="nonzero"><polygon points="0 18 5 18 5 0 0 0"></polygon><path d="M15.6725178,0 L14.5534833,8.40846934 L13.8582008,3.83502426 C13.65661,2.37009263 13.4632474,1.09175121 13.278113,0 L7,0 L7,18 L11.2416347,18 L11.2580911,6.11380679 L13.0436094,18 L16.0633571,18 L17.7583653,5.8517865 L17.7707076,18 L22,18 L22,0 L15.6725178,0 Z"></path><path d="M24,18 L24,0 L31.8045586,0 C33.5693522,0 35,1.41994415 35,3.17660424 L35,14.8233958 C35,16.5777858 33.5716617,18 31.8045586,18 L24,18 Z M29.8322479,3.2395236 C29.6339219,3.13233348 29.2545158,3.08072342 28.7026524,3.08072342 L28.7026524,14.8914865 C29.4312846,14.8914865 29.8796736,14.7604764 30.0478195,14.4865461 C30.2159654,14.2165858 30.3021941,13.486105 30.3021941,12.2871637 L30.3021941,5.3078959 C30.3021941,4.49404499 30.272014,3.97397442 30.2159654,3.74371416 C30.1599168,3.5134539 30.0348852,3.34671372 29.8322479,3.2395236 Z"></path><path d="M44.4299079,4.50685823 L44.749518,4.50685823 C46.5447098,4.50685823 48,5.91267586 48,7.64486762 L48,14.8619906 C48,16.5950653 46.5451816,18 44.749518,18 L44.4299079,18 C43.3314617,18 42.3602746,17.4736618 41.7718697,16.6682739 L41.4838962,17.7687785 L37,17.7687785 L37,0 L41.7843263,0 L41.7843263,5.78053556 C42.4024982,5.01015739 43.3551514,4.50685823 44.4299079,4.50685823 Z M43.4055679,13.2842155 L43.4055679,9.01907814 C43.4055679,8.31433946 43.3603268,7.85185468 43.2660746,7.63896485 C43.1718224,7.42607505 42.7955881,7.2893916 42.5316822,7.2893916 C42.267776,7.2893916 41.8607934,7.40047379 41.7816216,7.58767002 L41.7816216,9.01907814 L41.7816216,13.4207851 L41.7816216,14.8074788 C41.8721037,15.0130276 42.2602358,15.1274059 42.5316822,15.1274059 C42.8031285,15.1274059 43.1982131,15.0166981 43.281155,14.8074788 C43.3640968,14.5982595 43.4055679,14.0880581 43.4055679,13.2842155 Z"></path></g></svg>
+          </a></li>
+        </ul>
+      </div>
+      <div class="arrow" @click="scrollToTop" v-if="showArrow">⇧</div>
+      <div class="buttons">
+        <base-button dark><router-link to="/">Add to Watch List</router-link></base-button>
       </div>
     </div>
   </div>
@@ -52,7 +60,7 @@
 
 
 <script setup>
-import { defineProps, reactive, computed } from 'vue';
+import { defineProps, reactive, computed, ref } from 'vue';
 import { useStore } from 'vuex';
 
 const store = useStore()
@@ -76,16 +84,24 @@ const moneyToString = (money) => {
   return String(money).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, '$1,')
 }
 
+const scrollToTop = ()=> {
+  window.scrollTo(0, 0)
+}
+
+let showArrow = ref(false)
+
+window.onscroll = ()=> {
+  showArrow.value = window.scrollY > window.innerHeight
+}
+
 async function getDetails() {
   await store.dispatch('getDetails', props.id)
   data.movie = reactive(store.state.recipesDetails.find(obj => obj.id === +props.id))
-  console.log(data)
   }
  getDetails()
 
  store.dispatch('getGenresList')
  
-
 </script>
 
 <style lang="scss" scoped>
@@ -100,44 +116,136 @@ async function getDetails() {
     display: flex;
     flex-direction: column;
     align-items: center;
-    .data {
-      padding-left: 3%;
-      .title {
-        font-family: 'Merriweather', serif;
-        margin-bottom: 0;
-      }
-      .original_title {
-        margin-top: 5px;
-        .lang {
-          text-transform: uppercase;
-          margin-right: 5px;
-          border: 1px solid #2B3467;
-          padding: 2px 4px;
+    padding-left: 3%;
+    .title {
+      font-family: 'Acme', sans-serif;
+      font-size: 3rem;
+      letter-spacing: 1px;
+      text-align: center;
+    }
+    .original_title {
+      margin-top: -25px;
+      font-size: 2rem;
+    }
+    .lang {
+      text-transform: uppercase;
+      margin-right: 10px;
+      border: 1px solid #2B3467;
+      padding: 2px 4px;
+    }
+    img {
+      max-height: 1000px;
+      margin-bottom: 50px;
+    }
+    .badges {
+      margin: 30px 0;
+    }
+    .overview.box {
+      margin-top: 50px;
+    }
+    .box {
+      box-shadow: 0 0 3px 1px #2B3467;
+      padding: 3%;
+      width: 100%;
+      margin-top: 10px;
+      ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        li {
+          margin-bottom: 10px;
         }
       }
-      .badges {
-        margin: 30px 0;
+    }
+    table {
+      margin-top: 10px;
+      th {
+        text-align: left;
+        padding-right: 10px;
       }
-      .overview {
-        margin-top: 0;
-        box-shadow: 0 0 3px 1px #2B3467;
+      .rating {
+        display: inline-block;
+        background-color: rgb(202, 202, 77);
+        color: #fff;
+        padding: 3px 5px;
+        border-radius: 15px;
+        &.high {
+        background-color: rgb(95, 218, 95);
+        }
+        &.low {
+          background-color: #EB455F;
+        }
+      }
+    }
+    .companies {
+      display: flex;
+      flex-wrap: wrap;
+      .box_header {
+        width: 100%;
+        margin-bottom: 20px;
+      }
+      .company {
+        display: grid;
+        border: 1px solid black;
         padding: 3%;
-      }
-      .votes {
-        .rating {
-          display: inline-block;
-          background-color: rgb(202, 202, 77);
-          color: #fff;
-          padding: 3px 5px;
-          border-radius: 15px;
-          &.high {
-            background-color: rgb(95, 218, 95);
-          }
-          &.low {
-            background-color: #EB455F;
-          }
+        border-radius: 10px;
+        margin-right: 5px;
+        margin-bottom: 5px;
+        min-width: 250px;
+        min-height: 250px;
+        img {
+          justify-self: center;
+          max-width: 150px;
+          max-height: 150px;
         }
       }
+    }
+    .countries table {
+      th {
+        font-weight: normal;
+        text-align: center;
+      }
+      td {
+        padding: 5px 0;
+      }
+    }
+    .languages ul {
+      margin: 16px;
+      list-style:disc;
+    }
+    .links li {
+      font-weight: bold;
+      font-size: 1.3rem;
+      transform-origin: left center;
+      transition: transform .1s ease-in-out;
+      &:hover {
+        transform: scale(1.2);
+      }
+    }
+    .arrow {
+      position: fixed;
+      bottom: 50px;
+      right: 50px;
+      font-size: 5rem;
+      border: 3px solid #2B3467;
+      border-radius: 50%;
+      color: #2B3467;
+      width: 110px;
+      height: 110px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      transition: transform .3s ease-out;
+      background-color: #BAD7E9;
+      &:hover {
+        color: #EB455F;
+        border-color: #EB455F;
+        transform: translateY(-30px);
+      }
+    }
+    .buttons {
+      margin-top: 50px;
     }
   }
 }
