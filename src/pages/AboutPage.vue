202e54fdf908bf3dc07376270114d05608ffda42
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-03-21 19:34:49 +0100

    Add content

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <h1>About Page</h1>
+    <h2>Few words about this project</h2>
+    <section class="max-width">
+      <p><em>Pick A Movie</em> can be a convenient tool for movie enthusiasts. Users can search for movies by title, genre or any other relevant criteria. The page can also provide information about each movie, such as its plot summary and rating.</p>
+      <p>By creating an account on the page, users can save movies to their personal watch list, which can be accessed at any time. This feature allows users to keep track of movies they want to watch in the future and makes it easy to find and watch them when the time is right.</p>
+      <p>Overall, <em>Pick A Movie</em> is a convenient and practical tool for movie lovers who want to discover new films and keep track of their favorites.</p>
+    </section>
   </div>
 </template>
 
@@ -9,5 +14,16 @@
 </script>
 
 <style lang="scss" scoped>
-
+section {
+  font-size: 1.2rem;
+  background-color: #FCFFE7;
+  padding: 3%;
+  margin: 15px auto;
+  border-width: 5px;
+  border-style: solid;
+  border-image: linear-gradient(to right, #2B3467, #EB455F) 1;
+  em {
+    font-weight: bold;
+  }
+}
 </style>
