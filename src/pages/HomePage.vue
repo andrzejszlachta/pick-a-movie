5f54edb6ee5134d0342f9e8ff2a36eeb40a7f433
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-03-21 20:32:35 +0100

    Add content

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-    <h1>Home Page</h1>
+    <h2>Welcome to Pick A Movie!</h2>
+    <section class="max-width">
+      <p>Welcome to our website about movies, your one-stop destination for all things related to movies! Our website is designed to provide movie enthusiasts with an easy and enjoyable browsing experience, as well as offer valuable information and resources about movies.</p>
+      <p>You'll find here a <em>Movie Picker</em> that allows you to quickly find movies you're interested in. You can browse movies by genres such as action, drama, comedy, horror, or romance, or filter them by release date or rating.</p>
+      <p>Our featured content section highlights some of the latest or most popular movies, giving you a sense of what's currently trending.</p>
+      <p>Having an account also allows to create your own personalized watch list. Add and remove movies to your list as you like and never forget to watch that great movie again!</p>
+      <p>Whether you're a casual moviegoer or a die-hard film fan, our website has something for everyone. So, start exploring and discovering new movies today!</p>
+    </section>
   </div>
 </template>
 
-<script setup>
-// import { useStore } from 'vuex';
-// import { onMounted } from 'vue';
-
-// const store = useStore();
-
-// onMounted(() => {
-//   store.dispatch('getTrendingMoviesList')
-// });
-
-</script>
 <style lang="scss" scoped>
-
+section {
+  font-size: 1.2rem;
+  background-color: #FCFFE7;
+  padding: 3%;
+  margin: 15px auto;
+  border-width: 5px;
+  border-style: solid;
+  border-image: linear-gradient(to right, #2B3467, #EB455F) 1;
+}
 </style>
